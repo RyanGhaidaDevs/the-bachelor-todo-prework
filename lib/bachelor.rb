894cf9101a85fc715a53c632a044c04a8899bdab
Ryan Ghaida <ryaboug@gmail.com>
2018-04-14 19:53:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,7 +16,13 @@ name = ""
 end
 
 def get_contestant_name(data, occupation)
-  
+  data.each do |contestant|
+
+    contestant.each do |key, value|
+
+      if value == occupation
+
+        return contestants["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
