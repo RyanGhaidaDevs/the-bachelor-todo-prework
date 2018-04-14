c8d13d07bd07d7aa09d4af4cc9ec9b183153d479
Ryan Ghaida <ryaboug@gmail.com>
2018-04-14 19:47:51 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,7 @@ def get_first_name_of_season_winner(data, season)
       contestants.each do |key, value|
         if value == "winner"
 
-          return key
+          return key["name"]
         end
       end 
 
